35201153df64eb37712db64d497e46671f349f1c
hemanth5055 <kanalahemanthrdy@gmail.com>
2025-05-31 21:24:50 +0530

    fixed cookies

--- backend/Utils/generateTokenAndSetCookie.js
+++ backend/Utils/generateTokenAndSetCookie.js
@@ -5,6 +5,7 @@ export const generateTokenAndSetCookie = (res, id) => {
   });
   res.cookie("token", token, {
     httpOnly: true,
+    sameSite:"none",
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
